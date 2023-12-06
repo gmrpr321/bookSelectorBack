7b13ee8b0597cd402788b141cf2776550b924163
pradeep sn <pradeepsn606@gmail.com>
2023-12-07 01:17:17 +0530

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const DatabaseUtil = require("./databaseUtil");
-const PORT = 8080;
+const PORT = process.env.PORT | 8080;
 const app = express();
 app.use(cors());
 app.use(express.json());
